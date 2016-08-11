f33691972304937c688db0488c5778f20c7d73ac
Havallon <havallon.ecomp@gmail.com>
2016-08-10 22:51:37 -0300

    Classe servidor e Sistema com novos metodos

--- src/Model/Servidor.java
+++ src/Model/Servidor.java
@@ -1,27 +1,39 @@
 package Model;
 
+import Model.Util.Sistema;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Servidor {
-
+    
     private final ArrayList<Cliente> clientes;
     private ArrayList<Usuario> usuarios;
-    private int port;
-
-    public Servidor(int port) {
+    private final int port;
+    
+    public Servidor(int port) throws IOException, ClassNotFoundException {
         this.port = port;
         clientes = new ArrayList<>();
-        usuarios = new ArrayList<>();
+
+        //Carregando os usuarios do sistema
+        try {
+            usuarios = (ArrayList<Usuario>) Sistema.CarregarSistema();
+        } catch (FileNotFoundException ex) {
+            //caso o arquivo não exista cria um novo.
+            usuarios = new ArrayList<>();
+            Sistema.SalvarSistema(usuarios);
+        }
     }
 
     //Ligando o servidor
     public void ligarServidor() throws IOException {
         ServerSocket server = new ServerSocket(port);
         System.out.println("Servidor Iniciado com sucesso");
-
+        
         while (true) {
             Socket cliente = server.accept();
             Cliente c = new Cliente(cliente, this);
@@ -30,7 +42,7 @@ public class Servidor {
             new Thread(c).start();
         }
     }
-    
+
     //desconectando o cliente
     public void desconectarCliente(Cliente c) {
         if (!clientes.isEmpty()) {
@@ -39,9 +51,9 @@ public class Servidor {
         }
     }
     
-    private void cadastrar(String nome, String senha, Cliente c){
-        for (Usuario u : usuarios){
-            if (u.getName().equals(nome)){
+    private void cadastrar(String nome, String senha, Cliente c) throws IOException {
+        for (Usuario u : usuarios) {
+            if (u.getName().equals(nome)) {
                 //enviar a msg de erro de acordo com o protocolo
                 return;
             }
@@ -49,11 +61,11 @@ public class Servidor {
         Usuario u = new Usuario(nome, senha);
         usuarios.add(u);
         //enviar msg para o cliente informando que o usuario foi cadastrado
-        
+        Sistema.SalvarSistema(usuarios);
     }
-    
+
     //Tratando todas as msgs em que o cliente manda para o servidor
     public void tratarMsg(String msg, Cliente cliente) {
-
+        
     }
 }
--- src/Model/Util/Sistema.java
+++ src/Model/Util/Sistema.java
@@ -1,5 +1,25 @@
 package Model.Util;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class Sistema {
-    
+
+    public static void SalvarSistema(Object obj) throws FileNotFoundException, IOException {
+        FileOutputStream fos = new FileOutputStream("dados.ld");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(obj);
+        oos.close();
+        fos.close();
+    }
+
+    public static Object CarregarSistema() throws IOException, ClassNotFoundException {
+        FileInputStream fis = new FileInputStream("dados.ld");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        return ois.readObject();
+    }
 }
