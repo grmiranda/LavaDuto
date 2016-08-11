28d5b75fb53c5083f67fdc7e8ebd742494589632
Gabriel Reis Miranda <grmiranda97@gmail.com>
2016-08-10 22:24:45 -0300

    Correção na classe Arquivo, lista de IpOrigem

--- src/Model/Arquivo.java
+++ src/Model/Arquivo.java
@@ -1,19 +1,29 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Arquivo {
     private final String name;
-    private final String ipOrigin;
+    private ArrayList<String> ipOrigem = new ArrayList<>();
 
-    public Arquivo(String name, String ipOrigin) {
+    public Arquivo(String name, String ipOrigem) {
         this.name = name;
-        this.ipOrigin = ipOrigin;
+        this.ipOrigem.add(ipOrigem);
     }
 
     public String getName() {
         return name;
     }
 
-    public String getIpOrigin() {
-        return ipOrigin;
+    public ArrayList<String> getIpOrigem() {
+        return ipOrigem;
+    }
+    
+    public void adicionarIpOrigem(String ipOrigem){
+        this.ipOrigem.add(ipOrigem);
+    }
+    
+    public void removerIpOrigem(String ipOrigem){
+        this.ipOrigem.remove(ipOrigem);
     }
 }
